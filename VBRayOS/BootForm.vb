95f5eb74208d84f090a02856af7ab1da3d57ee2a
VBRayOS Developer <developer@vbrayos.com>
2013-12-06 18:00:00 +0900

    Complete boot form with progress animation and transition to login

--- VBRayOS/BootForm.vb
+++ VBRayOS/BootForm.vb
@@ -42,4 +42,28 @@ Public Class BootForm
 
         PositionControls()
     End Sub
+
+    Private Sub PositionControls()
+        ' 컨트롤들을 화면 중앙에 배치
+        logoLabel.Location = New Point((Me.Width - logoLabel.Width) \ 2, (Me.Height \ 2) - 100)
+        progressBar.Location = New Point((Me.Width - progressBar.Width) \ 2, (Me.Height \ 2) + 50)
+    End Sub
+
+    Private Sub bootTimer_Tick(sender As Object, e As EventArgs) Handles bootTimer.Tick
+        progressBar.Value += 2
+        
+        If progressBar.Value >= 100 Then
+            bootTimer.Enabled = False
+            ' 로그인 폼으로 전환
+            Dim loginForm As New LoginForm()
+            loginForm.Show()
+            Me.Hide()
+        End If
+    End Sub
+
+    Private Sub BootForm_Resize(sender As Object, e As EventArgs) Handles Me.Resize
+        If logoLabel IsNot Nothing AndAlso progressBar IsNot Nothing Then
+            PositionControls()
+        End If
+    End Sub
 End Class
